f39edec870648b9b7af58bf41e4692125021c1b5
Jonathan <jonathanyin.12@gmail.com>
2025-02-05 11:24:41 -0500

    open links in new tab

--- frontend/src/components/ui/chat/message.tsx
+++ frontend/src/components/ui/chat/message.tsx
@@ -10,7 +10,9 @@ const markdownComponents = {
   h2: ({ ...props }) => <h2 className="text-2xl font-semibold" {...props} />,
   h3: ({ ...props }) => <h3 className="text-xl font-semibold" {...props} />,
   h4: ({ ...props }) => <h4 className="text-lg font-semibold" {...props} />,
-  a: ({ ...props }) => <a className="text-blue-500 underline" {...props} />,
+  a: ({ ...props }) => (
+    <a className="text-blue-500 underline" target="_blank" {...props} />
+  ),
 };
 
 export const AgentMessage = ({ message }: MessageProps) => {
