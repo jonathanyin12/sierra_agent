02b6470eae44511f4dbe1f600dba54d9c25e5ba3
Jonathan <jonathanyin.12@gmail.com>
2025-02-04 21:47:50 -0500

    basic ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,101 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { CornerDownLeft } from "lucide-react";
+import {
+  ChatBubble,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/components/ui/chat/chat-bubble";
+import { ChatInput } from "@/components/ui/chat/chat-input";
+import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      // if (isGenerating || isLoading || !input) return;
+      // setIsGenerating(true);
+      // onSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+    }
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [input, setInput] = useState("");
+
+  return (
+    <div
+      style={{
+        maxWidth: "1000px",
+        margin: "0 auto",
+        height: "100dvh",
+        maxHeight: "100dvh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <ChatMessageList>
+        <ChatBubble variant="sent">
+          <ChatBubbleAvatar fallback="US" />
+          <ChatBubbleMessage variant="sent">
+            Hello, how has your day been? I hope you are doing well.
+          </ChatBubbleMessage>
+        </ChatBubble>
+        <ChatBubble variant="received">
+          <ChatBubbleAvatar fallback="AI" />
+          <ChatBubbleMessage variant="received">
+            Hi, I am doing well, thank you for asking. How can I help you today?
+          </ChatBubbleMessage>
+        </ChatBubble>
+        <ChatBubble variant="sent">
+          <ChatBubbleAvatar fallback="US" />
+          <ChatBubbleMessage variant="sent">
+            Hello, how has your day been? I hope you are doing well.
+          </ChatBubbleMessage>
+        </ChatBubble>
+        <ChatBubble variant="received">
+          <ChatBubbleAvatar fallback="AI" />
+          <ChatBubbleMessage variant="received">
+            Hi, I am doing well, thank you for asking. How can I help you today?
+          </ChatBubbleMessage>
+        </ChatBubble>
+        <ChatBubble variant="sent">
+          <ChatBubbleAvatar fallback="US" />
+          <ChatBubbleMessage variant="sent">
+            Hello, how has your day been? I hope you are doing well.
+          </ChatBubbleMessage>
+        </ChatBubble>
+        <ChatBubble variant="received">
+          <ChatBubbleAvatar fallback="AI" />
+          <ChatBubbleMessage variant="received">
+            Hi, I am doing well, thank you for asking. How can I help you today?
+          </ChatBubbleMessage>
+        </ChatBubble>
+        {/* <ChatBubble variant="received">
+          <ChatBubbleAvatar fallback="AI" />
+          <ChatBubbleMessage isLoading />
+        </ChatBubble> */}
+      </ChatMessageList>
+      <div className="m-4 relative rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring">
+        <ChatInput
+          placeholder="Message Sierra Outfitters"
+          value={input}
+          onKeyDown={onKeyDown}
+          onChange={(e) => setInput(e.target.value)}
+          className="rounded-lg bg-background border-0 shadow-none focus-visible:ring-0"
+        ></ChatInput>
+        <div className="flex items-center p-3 pt-0">
+          <Button
+            disabled={!input}
+            type="submit"
+            size="sm"
+            className="ml-auto gap-1.5"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Send Message
+            <CornerDownLeft className="size-3.5" />
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
